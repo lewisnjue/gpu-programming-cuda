5a4b4fc185155c11ddaa60b9baf048867ac3cefc
lewis njue <lewiskinyuanjue.ke@gmail.com>
2026-04-24 21:10:08 +0000

    added command to run with docker

--- running.sh
+++ running.sh
@@ -2,3 +2,8 @@
 
 # to combine for the T4 gpu use the arch argument like this for example 
 nvcc -arch=sm_75 hello.cu -o hello
+# if you want to run in docker you can use this command 
+
+docker run -it --rm --gpus all -v ./:/code nvidia/cuda:12.0.0-devel-ubuntu20.04 bash 
+
+
